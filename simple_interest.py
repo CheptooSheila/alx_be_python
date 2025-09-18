e380fe248c364677d3393e6ba00ce1b42973bf7d
CheptooSheila <cheptoosheila177@gmail.com>
2025-09-18 09:01:32 -0700

    Simple interest

---
+++ simple_interest.py
@@ -0,0 +1,9 @@
+#A script calculating simple interest in dollars
+P = 1000 #In dollars
+R = 0.05
+T = 3 #years
+
+#The formula for simple interest
+interest = P * R * T
+
+print("The simple interest is: ", interest)
